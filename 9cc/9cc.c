f230ca99a13d0009b6a38f45e364a36db1f8177c
Yuku Takahashi <taka84u9@gmail.com>
2020-06-20 21:33:20 +0900

    Implement unary

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -40,6 +40,7 @@ Node *new_node(NodeKind kind, Node *lhs, Node *rhs);
 Node *new_node_num(int val);
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 void gen(Node *node);
 
@@ -197,16 +198,26 @@ Node *expr() {
 }
 
 Node *mul() {
-  Node *node = primary();
+  Node *node = unary();
 
   while (true) {
     if (consume('*')) {
-      node = new_node(ND_MUL, node, primary());
+      node = new_node(ND_MUL, node, unary());
     } else if (consume('/')) {
-      node = new_node(ND_DIV, node, primary());
+      node = new_node(ND_DIV, node, unary());
     } else {
       return node;
     }
+  }
+}
+
+Node *unary() {
+  if (consume('+')) {
+    return primary();
+  } else if (consume('-')) {
+    return new_node(ND_SUB, new_node_num(0), primary());
+  } else {
+    return primary();
   }
 }
 
