b7359411408106ff4941e784662f9f9a416201f5
Yuku Takahashi <taka84u9@gmail.com>
2021-11-05 23:01:17 +0900

    Strapi app

---
+++ strapi/user-with-relation/app/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'dec488b212c7ccd5129186d821f8f28b'),
+    },
+  },
+});
